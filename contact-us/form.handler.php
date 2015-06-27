a9306129b635e9364210c4f1ff246146b0062120
William Naughton-Gravette <William@Williams-MBP-3.gateway>
2015-06-27 23:06:11 +1000

    SQL INSERT update

--- contact-us/form.handler.php
+++ contact-us/form.handler.php
@@ -1,4 +1,3 @@
-<pre>
 <?php
 	
 	$mysqli = new mysqli("localhost", "root", "", "educate-front");
@@ -7,11 +6,11 @@
 	$schoolName = $mysqli->real_escape_string($_POST['schoolName']);
 	$schoolPop = $mysqli->real_escape_string($_POST['schoolPop']);
 	$schoolLoc = $mysqli->real_escape_string($_POST['schoolLoc']);
+	$phone = $mysqli->real_escape_string($_POST['phone']);
 	$email = $mysqli->real_escape_string($_POST['email']);
 	$personType = $mysqli->real_escape_string($_POST['personType']);
 	$state = $mysqli->real_escape_string($_POST['state']);
 	$actionReq = $mysqli->real_escape_string($_POST['actionReq']);
 	
-	$mysqli->query("INSERT INTO `inq` (name, schoolName, schoolPop, schoolLoc, email, personType, state, actionReq) VALUES ('$name', '$schoolName', '$schoolPop', '$schoolLoc', '$email', '$personType', '$state', '$actionReq')");	
+	$mysqli->query("INSERT INTO `inq` (name, schoolName, schoolPop, schoolLoc, phone, email, personType, state, actionReq) VALUES ('$name', '$schoolName', '$schoolPop', '$schoolLoc', '$phone', '$email', '$personType', '$state', '$actionReq')");	
 ?>
-</pre>
