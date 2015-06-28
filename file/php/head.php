fe05e6fee8919af0bc0e303d821740f80fdfeb05
William Naughton-Gravette <William@Williams-MBP-3.gateway>
2015-06-28 20:00:17 +1000

    fixed base URL

--- file/php/head.php
+++ file/php/head.php
@@ -1,3 +1,4 @@
+<base href="/"/>
 <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 <link rel="stylesheet"  href="file/css/global.css"/>
